00792c9ef161f79dacecaf109901f87b7a3b10a1
Lu Yuan <lyzczy126@126.com>
2015-04-22 18:52:45 +0800

    add redis
    Signed-off-by: Lu Yuan <lyzczy126@126.com>

--- filter_data.py
+++ filter_data.py
@@ -1,17 +1,24 @@
 import csv
 import redis
-import itertools
+import json
 
 
-if __name__ == "__main__":
-    with open('/home/hadoop/ly/603998', 'rb') as file:
-        reader = csv.DictReader(file)
+def filter_data(stock_id, stock_directory):
+    path = stock_directory + stock_id
+    with open(path, 'rb') as local_file:
+        reader = csv.DictReader(local_file)
         per_day_data_list = []
         for row in reader:
             if "000" == row['Volume']:
-                continue 
+                continue
             else:
                 per_day_data_list.append(row)
-        print per_day_data_list[0]
-        #chunks = itertools.groupby(reader, keyfunc)
-        #print list(chunks)[0]
+        return per_day_data_list
+
+
+if __name__ == "__main__":
+    pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+    db = redis.StrictRedis(connection_pool=pool)
+    value = filter_data('603998', '/home/hadoop/ly/')
+    json_value = json.dumps(value)
+    db.set('603998', json_value)
